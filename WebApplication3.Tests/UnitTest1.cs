a25fa2eace7a9e62b3ae0e651982fbb3369e0fa3
Bill Campbell <billc@simplicityhealthsystems.com>
2015-01-16 11:11:35 -0600

    Full commit
    Full commit of project

---
+++ WebApplication3.Tests/UnitTest1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Controllers;
+using WebApplication3.Models;
+
+namespace WebApplication3.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetProduct()
+        {
+            //Setup
+            var controller = new ProductController();
+            List<Product> products = (new List<Product>(controller.products));
+ 
+            //Action
+            var result = controller.GetProduct(2) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content.Name, products[1].Name );
+
+
+
+        }
+
+        [TestMethod]
+        public void GetProduct_ShouldReturnWrongProduct()
+        {
+            var controller = new ProductController();
+            List<Product> products = new List<Product>(controller.products);
+
+            var result = controller.GetProduct(1) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(result.Content.Name, products[1]);
+
+        }
+
+        [TestMethod]
+        public async Task GetProductAsync_ShouldReturnCorrectProduct()
+        {
+            var pIndex = 1;
+            var controller = new ProductController();
+            List<Product> products = new List<Product>(controller.products);
+
+            var result = await controller.GetProductAsync(pIndex) as OkNegotiatedContentResult<Product>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content.Name, products[pIndex-1].Name);
+
+
+
+        }
+
+
+        [TestMethod]
+        public void SaveProduct_ShouldReturnSimpleOK()
+        {
+            var p = new Product();
+            p.Name = "test";
+            p.Id = 4;
+            p.Category = "testcat";
+
+            var controller = new ProductController();
+            Task<IHttpActionResult> response = controller.SaveProduct(p);
+
+            Assert.IsNotNull(response);
+            Assert.IsInstanceOfType(response.Result, typeof(OkResult));
+
+        }
+
+        [TestMethod]
+        public void TestGetAllAgain_ShouldReturnAllProducts()
+        {
+            var controller = new ProductController();
+            Task<IHttpActionResult> response = controller.Func1GetAllOkAsync();
+            List<Product> testList = new List<Product>();
+
+            Assert.IsNotNull(response);
+            Assert.IsInstanceOfType(response.Result, typeof(OkNegotiatedContentResult<IEnumerable<Product>>));
+
+
+        }
+
+
+        //[TestMethod]
+        //public void GetProductList_ShouldReturnAllProducts()
+        //{
+        //    var controller = new ProductController();
+        //    IEnumerable<Product> products = new List<Product>(controller.products);
+
+        //    var response = controller.GetProductList();
+        //    Assert.IsNotNull(response);
+        //    Assert.AreEqual(response, products);
+
+        //}
+
+    }
+}
---
+++ WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebApplication3.Models;
+using System.Web.Http.Cors;
+
+namespace WebApplication3.Controllers
+{
+    [EnableCors(origins: "http://localhost", headers: "*", methods: "*")]
+    public class ProductController : ApiController
+    {
+        public Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<Product> GetProductList()
+        //{
+        //    return products.ToList();
+        //}
+            
+            
+        [HttpGet]
+        public Product _GetSpecific()
+        {
+            return products[1];
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetProduct(int id)
+        {
+            //Task<string> returnedTaskResult =  TestStringAsync(name: "BillJ", size: 23);
+            //var result = returnedTaskResult.Result;
+            //string strResult = await returnedTaskResult;
+            //return returnedTaskResult.Result;
+
+           
+
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+                return NotFound();
+            else
+                return Ok(product );
+
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetProductAsync(int id)
+        {
+            //Task<string> returnedTaskResult = TestStringAsync(name: "BillJ", size: 23);
+            //var result = returnedTaskResult.Result;
+            //string strResult = await returnedTaskResult;
+            //return returnedTaskResult.Result;
+
+            //var x = await Task.FromResult(GetProduct(id));
+
+
+            //var product = products.FirstOrDefault((p) => p.Id == id);
+            //if (product == null)
+            //    return NotFound();
+            //else
+            //    return  Ok(x);
+
+            return await Task.FromResult(GetProduct(id));
+
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Func1GetAllOkAsync()
+        {
+            var x = await Task.FromResult(GetAllProducts());
+
+            return Ok(x);
+        }
+
+
+
+
+
+        [HttpPost]
+        public async Task<IHttpActionResult> SaveProduct(Product p)
+        {
+            return Ok();
+            //return InternalServerError();
+        }
+
+
+        async Task<string> TestStringAsync(int size, string name)
+        {
+            int subResult = await TestSubAsynTask(3);
+
+
+
+            var s = string.Format("Size = {0} =>{2}, Name = {1}", size, name, subResult);
+
+            return s;
+
+        }
+
+        async Task<int> TestSubAsynTask(int input)
+        {
+
+            return input + 1;
+        }
+
+        async Task<string> TestSub3Task(string input)
+        {
+            return input + " hi";
+        }
+
+        async Task TestAboveTask()
+        {
+            var x = await Task.FromResult(TestSub3Task("Bill"));
+            string s = x.Result;
+        }
+
+      
+
+
+    }
+}
